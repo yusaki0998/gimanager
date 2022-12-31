0bb5a1984b4aa57f23b54b82c6d2c9a3b5093ce0
Yusaki Thanh Trung <yusakithejoker@Yusakis-MacBook-Pro.local>
2022-12-31 10:10:48 +0700

    Update

--- Gemfile
+++ Gemfile
@@ -28,6 +28,10 @@ gem "cocoon"
 gem "tabs_on_rails"
 gem 'carrierwave'
 gem 'cancancan'
+gem 'capistrano', '~> 3.11'
+gem 'capistrano-rails', '~> 1.4'
+gem 'capistrano-passenger', '~> 0.2.0'
+gem 'capistrano-rbenv', '~> 2.1', '>= 2.1.4'
 group :development, :test do
   gem 'sqlite3'
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default'],
-    toastr: 'toastr/toastr',
-    ApexCharts: ['apexcharts', 'default'],
-    underscore: ['underscore', 'm'],
-    Rails: ['@rails/ujs']
-  })
-)
 module.exports = environment
